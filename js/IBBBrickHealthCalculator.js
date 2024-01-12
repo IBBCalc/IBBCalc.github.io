16791a37e861ed6339fe87c40bf2e8aba1d4848a
IBBCalc <147992650+IBBCalc@users.noreply.github.com>
2024-01-12 14:40:16 +0100

    v2.1.1 Can now add and remove brick hp rows

--- js/IBBBrickHealthCalculator.js
+++ js/IBBBrickHealthCalculator.js
@@ -27,6 +27,16 @@ function StoreItem(key, obj) {
 	window.localStorage.setItem(key, JSON.stringify(obj));
 }
 
+function handleNewRow(e) {
+	var newobj = {
+		'level': null
+	};
+	data.push(newobj);
+	console.log('Added new row')
+	StoreItem(storagename, data);
+	$('#table_brickhealth').DataTable().row.add(newobj).draw();
+}
+
 // %%%%%%%%%%%%%%%%%%%%%%% Update Data methods %%%%%%%%%%%%%%%%%%%%%%%
 
 function UpdateNumber(data, e) {
@@ -42,7 +52,7 @@ function UpdateNumber(data, e) {
 // %%%%%%%%%%%%%%%%%%%%%%% Created Cell Templates %%%%%%%%%%%%%%%%%%%%%%%
 
 function RefreshDataCells() {
-	var datacells = [1,2,3];
+	var datacells = [1,2,3,4];
 	$('#table_brickhealth').DataTable().cells(null, datacells).invalidate();
 }
 
@@ -51,6 +61,18 @@ function CreatedNumberTemplate(data) {
 		cell.addEventListener('change', function(e) {
 			UpdateNumber(data, e);
 			RefreshDataCells();
+		});
+	}
+}
+
+function CreatedDeleteTemplate(data) {
+	return function(cell, celldata, rowdata, rowindex, colindex) {
+		cell.addEventListener('click', function(e) {
+			var r = e.currentTarget._DT_CellIndex.row;
+			console.log('Delete row: ', data[r])
+			data.splice(r, 1)
+			StoreItem(storagename, data);
+			$('#table_brickhealth').DataTable().row(this).remove().draw()
 		});
 	}
 }
@@ -69,7 +91,7 @@ function ColumnNumberTemplate(data, title, width) {
 			}
 
 			if ( type === 'display' ) {
-				return '<input type="text" step="1" value="' + data + '">';
+				return '<input type="number" step="1" value="' + data + '">';
 			}
 			return data;
 		},  
@@ -97,6 +119,20 @@ function ColumnDataTemplate(func, title, width, format) {
 	};
 }
 
+function ColumnDeleteTemplate(title, width) {
+	return { 
+		title: title, 
+		width: width, 
+		defaultContent: '',
+		render: function(data, type, row, meta) {
+			if ( type === 'display' ) {
+				return '<a class="abutton" href="#">X</a>';
+			}
+			return data;
+		},
+	};
+}
+
 // %%%%%%%%%%%%%%%%%%%%%%% Build Page %%%%%%%%%%%%%%%%%%%%%%%
 
 function BuildPage() {
@@ -119,6 +155,9 @@ function BuildTable() {
 		columnDefs: [{ 
 			targets: [0],
 			createdCell: CreatedNumberTemplate(data)
+		},{
+			targets: [5],
+			createdCell: CreatedDeleteTemplate(data)
 		}],
 		columns: [
 			ColumnNumberTemplate('level', 'Level', '120px'),
@@ -127,6 +166,7 @@ function BuildTable() {
 			ColumnDataTemplate(CalculateHexShieldHP, 'Shielded Hex eHP', '80px', true),
 			ColumnDataTemplate(CalculateHexShieldPenHP, 'Shielded Shield Pen Hex eHP', '80px', true),
 			//ColumnDataTemplate(CalculateRatio, 'Ratio (to X)', '80px', true),
+			ColumnDeleteTemplate('', '30px'),
 		],
 	});
 }
@@ -134,7 +174,7 @@ function BuildTable() {
 // %%%%%%%%%%%%%%%%%%%%%%% Calculations %%%%%%%%%%%%%%%%%%%%%%%
 
 function CalculateGreenHP(row, rowindex) {
-	if (row === undefined || row.level === null) {
+	if (row === undefined || row.level === undefined || row.level === null) {
 		return null;
 	}
 	
@@ -142,7 +182,7 @@ function CalculateGreenHP(row, rowindex) {
 }
 
 function CalculateHexHP(row, rowindex) {
-	if (row === undefined || row.level === null) {
+	if (row === undefined || row.level === undefined || row.level === null) {
 		return null;
 	}
 	
@@ -150,7 +190,11 @@ function CalculateHexHP(row, rowindex) {
 }
 
 function CalculateHexShieldHP(row, rowindex) {
-	if (row === undefined || row.level === null) {
+	if (row === undefined || row.level === undefined || row.level === null) {
+		return null;
+	}
+	
+	if (row.level < 600) {
 		return null;
 	}
 	
@@ -158,7 +202,11 @@ function CalculateHexShieldHP(row, rowindex) {
 }
 
 function CalculateHexShieldPenHP(row, rowindex) {
-	if (row === undefined || row.level === null) {
+	if (row === undefined || row.level === undefined || row.level === null) {
+		return null;
+	}
+	
+	if (row.level < 600) {
 		return null;
 	}
 	
--- js/version.js
+++ js/version.js
@@ -1,5 +1,5 @@
 $(document).ready( function () {
-    var version = 'Version 2.1.0'
+    var version = 'Version 2.1.1'
     //console.log(version);
     $('.version').html(version);
   });
